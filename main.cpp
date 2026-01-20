ba184e9036a9c240345743ea26945e5fa9d9de86
William Pepple <williampepple1@gmail.com>
2026-01-20 19:20:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
-#include <QCoreApplication>
+#include <QApplication>
+#include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QApplication app(argc, argv);
 
-    // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
-    // A not very useful example would be including
-    // #include <QTimer>
-    // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
-    // which quits the application after 5 seconds.
+    app.setApplicationName("Image Map Generator");
+    app.setApplicationVersion("1.0.0");
+    app.setOrganizationName("ImageCoord");
 
-    // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or use the Non-Qt Plain C++ Application template.
+    MainWindow window;
+    window.show();
 
-    return a.exec();
+    return app.exec();
 }
